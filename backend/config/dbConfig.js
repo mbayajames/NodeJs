10f0731f13c1f2777417602f2eda3a7122c58a5d
mbayajames <jmuthiora244@gmail.com>
2025-03-17 02:59:56 -0700

    Improved security measures in dbConfig.js for safer database connections

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,7 +1,17 @@
-module.exports={
+// Export an object containing database configuration settings
+module.exports = {
+    // The database host (e.g., 'localhost' or a remote server address)
     HOST: process.env.HOST,
+
+    // The username to connect to the database
     USER: process.env.USER,
+
+    // The password for the database user
     PASSWORD: process.env.PASSWORD,
+
+    // The name of the database
     DB: process.env.DATABASE,
+
+    // Specifies the type of database being used (MySQL in this case)
     dialect: 'mysql'
-}
+};
