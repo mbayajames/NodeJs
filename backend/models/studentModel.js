f1034a957886a503192c8c4bf81ffde87c43b0ba
mbayajames <jmuthiora244@gmail.com>
2025-03-18 02:03:09 -0700

    Modify studentModel.js to improve student schema and functionality

---
+++ backend/models/studentModel.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+    
+    const Student = sequelize.define("student", {
+        student_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+    });
+    return Student
+}
