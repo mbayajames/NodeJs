da64280ff04de2198590ab1f766f61f531c4af2e
mbayajames <jmuthiora244@gmail.com>
2025-03-20 10:47:54 -0700

    Modified index.js to enhance functionality and improve code structure

--- backend/index.js
+++ backend/index.js
@@ -18,14 +18,59 @@ var corOptions = {
 // Use CORS middleware with the specified options
 app.use(cors(corOptions));
 
+const studentRoute = require("./routes/studentRoutes");
+// const courseRoute = require("./routes/courseRoutes");
+// const authRoute = require("./routes/authRoute");
+
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
 // Middleware to parse URL-encoded data (from HTML forms)
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/Student', studentRoute);
+// app.use('/Course', courseRoute);
+// app.use('/auth', authRoute);
+
 // Set the server port from an environment variable or default to 5000
 const PORT = process.env.PORT || 5000;
+
+// handling 404 error
+// app.use(async(req, res, next) => {
+//     next(createError.NotFound())
+// })
+
+// Error handler
+// app.use((err, req, res, next) => {
+//     res.status(err.status || 500)
+//     res.send({
+//         error:{
+//             status: err.status || 500,
+//             message: err.message
+//         }
+//     })
+// }) 
+
+//ERROR HANDLING MIDDLEWARE
+app.use((err, req, res, next) => {
+    if(err.status === 401) {
+        res.status(401).send({
+            error: {
+                status: 401,
+                message: "Unauthorized: Invalid username/password"
+            }
+        })
+    } else {
+        //HANDLE OTHER ERRORS
+        res.status(err.status || 500).send({
+            error: {
+                status: err.status || 500,
+                message: err.message || "Internal Server Error"
+            }
+        });
+    }
+})
+
 
 // Start the Express server and listen on the specified port
 app.listen(PORT, () => {
